f77851bf39c5e1b5d611fe60b5bd2069a8d0d2ff
ninjab3381 <ninjab3381@gmail.com>
2020-07-03 13:51:14 -0700

    added Python plot

---
+++ Python/plot_exp_simulation.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+
+
+def read_file(filename):
+    ppm_list = []
+    rel_intensity_list = []
+    with open(filename, 'r') as f:
+        for line in f:
+            fields = line.split(",")
+            ppm = float(str(fields[0]).strip())
+            rel_intensity = float(str(fields[1]).strip())
+            ppm_list.append(ppm)
+            rel_intensity_list.append(rel_intensity)
+    f.close()
+    return ppm_list, rel_intensity_list
+
+
+def plot_line(ppm_list, rel_intensity_list, color, linestyle, label):
+    plt.plot(ppm_list, rel_intensity_list, color=color, linestyle=linestyle, label=label)
+
+
+if __name__ == "__main__":
+    temp = input("Enter Temp: ")
+    sigma = input("Enter Sigma in percent: ")
+    ppm_list1, rel_intensity_list1 = read_file("../Temp_213K/Experiment/213k.csv")
+    plot_line(ppm_list1, rel_intensity_list1, "green", "dashed", "Experiment")
+    ppm_list2, rel_intensity_list2 = read_file("../Temp_213K/Simulation_Spectra_Avg/213k_sigma_20pc_spectra_avg.csv")
+    plot_line(ppm_list2, rel_intensity_list2, "red", "dotted", "Avg. Simulation")
+    plt.xlabel('ppm')
+    plt.ylabel('relative intensity')
+    plt.suptitle('Experiment vs. Weighted Avg. Simulation Results')
+    plt.title('Temp = ' + temp + ' & Sigma = ' + sigma + '%')
+    plt.xlim(0, 150)
+    plt.legend(loc='upper right')
+    plt.show()
