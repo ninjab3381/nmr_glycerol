ddf3cd459aa2b3a48bd3806990646834d4911f25
ninjab3381 <ninjab3381@gmail.com>
2020-09-04 21:04:35 -0700

    added 1200Hz 15sd skewed plot 2

--- Python/plot_gauss_simulation.py
+++ Python/plot_gauss_simulation.py
@@ -22,7 +22,7 @@ def plot_line(ppm_list, rel_intensity_list, color, linestyle, label):
 if __name__ == "__main__":
     freq = input("Enter Jump Frequency: ")
     sigma = input("Enter Sigma in percent: ")
-    ppm_list2, rel_intensity_list2 = read_file("../GAUSS_SKEWED/jump_freq_1200/Simulation_Spectra_Avg/1200Hz_simulation_15sigma_lognormal_skewed1_for_plotting.csv")
+    ppm_list2, rel_intensity_list2 = read_file("../GAUSS_SKEWED/jump_freq_1200/Simulation_Spectra_Avg/1200Hz_simulation_15sigma_lognormal_skewed2_for_plotting.csv")
     fig, ax = plt.subplots()
     plot_line(ppm_list2, rel_intensity_list2, "red", "dotted", "Avg. Simulation")
     ax.set_xlabel('ppm')
