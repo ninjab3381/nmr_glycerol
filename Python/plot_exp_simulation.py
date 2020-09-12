d5b6acf100ef2ce6376574caec40c7164b0c1bf6
ninjab3381 <ninjab3381@gmail.com>
2020-09-11 23:07:36 -0700

    add digitized spectra files

--- Python/plot_exp_simulation.py
+++ Python/plot_exp_simulation.py
@@ -22,9 +22,9 @@ def plot_line(ppm_list, rel_intensity_list, color, linestyle, label):
 if __name__ == "__main__":
     freq = input("Enter Freq: ")
     sigma = input("Enter Sigma in percent: ")
-    ppm_list1, rel_intensity_list1 = read_file("../DIGITIZED_SPECTRA/jump_freq_800/DATA/dig_spectra_800.csv")
+    ppm_list1, rel_intensity_list1 = read_file("../DIGITIZED_SPECTRA/jump_freq_1200/DATA/dig_spectra_1200_v3.csv")
     plot_line(ppm_list1, rel_intensity_list1, "green", "dashed", "Experiment")
-    ppm_list2, rel_intensity_list2 = read_file("../GAUSS_SKEWED/jump_freq_800/Simulation_Spectra_Avg/800Hz_simulation_15sigma_lognormal_skewed1_for_plotting.csv")
+    ppm_list2, rel_intensity_list2 = read_file("../GAUSS_SKEWED/jump_freq_1200/Simulation_Spectra_Avg/1200Hz_simulation_05sigma_lognormal_skewed1_for_plotting.csv")
     plot_line(ppm_list2, rel_intensity_list2, "red", "dotted", "Avg. Simulation")
     plt.xlabel('ppm')
     plt.ylabel('relative intensity')
